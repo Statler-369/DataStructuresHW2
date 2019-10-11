cb761c94e4f698b128e7f28bfa455898276eba04
Statler Smith <statler.smith369@gmail.com>
2019-10-11 13:24:59 -0600

    Changes to Dictionary Controller

--- DataStructuresHW2/Controllers/DictionaryController.cs
+++ DataStructuresHW2/Controllers/DictionaryController.cs
@@ -1,5 +1,4 @@
-﻿using DataStructuresHW2.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,19 +8,38 @@ namespace DataStructuresHW2.Controllers
 {
     public class DictionaryController : Controller
     {
-        static Stack<string> myStack = new Stack<string>;
+        static Dictionary<string, int> myDict = new Dictionary<string, int>();
         // GET: Dictionary
         public ActionResult Index()
         {
-            
+
+            ViewBag.MyDictionary = myDict;
+
             return View();
         }
 
         public ActionResult AddOne()
         {
             //Add stuff to the stack. Not quite done yet. Watch the video to finish.
-            myStack.Push("Stack Item " + );
-            return ("Index");
+            myDict.Add("New entry " + (myDict.Count + 1), (myDict.Count + 1));
+
+            ViewBag.MyDictionary = myDict;
+
+            return View("Index");
+        }
+
+        public ActionResult AddHugeList()
+        {
+            //myDict.Clear();
+
+            for(int iCount = 0; iCount <= 2000; iCount ++)
+            {
+                myDict.Add("New entry " + (myDict.Count + 1), (myDict.Count + 1));
+            }
+
+            ViewBag.Dictionary = myDict;
+
+            return View("Index");
         }
     }
 }
