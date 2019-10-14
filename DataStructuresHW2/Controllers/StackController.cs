fec2e439a1b3b219f9c3adcc21e235818694034b
Statler Smith <statler.smith369@gmail.com>
2019-10-14 15:09:29 -0600

    Stack

--- DataStructuresHW2/Controllers/StackController.cs
+++ DataStructuresHW2/Controllers/StackController.cs
@@ -8,10 +8,38 @@ namespace DataStructuresHW2.Controllers
 {
     public class StackController : Controller
     {
+        static Stack<string> myStack = new Stack<string>;
         // GET: Stack
         public ActionResult Index()
         {
+
+            ViewBag.MyStack = myStack;
+
             return View();
+        }
+
+        public ActionResult AddOne()
+        {
+            myStack.Push("New entry " + (myStack.Count + 1));
+
+            ViewBag.MyStack = myStack;
+
+            return View("Index");
+        }
+
+        public ActionResult AddMany()
+        {
+            myStack.Clear();
+
+            for(int i = 0; i < 2000; i++)
+            {
+                myStack.Push("New entry " + (myStack.Count + 1));
+            }
+
+
+            ViewBag.MyStack = myStack;
+
+            return View("Index");
         }
     }
 }
